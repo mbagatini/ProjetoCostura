fff3ce41c733b446d3307f7663a005a6f4350250
Morgana <morganabagatini@gmail.com>
2017-08-24 21:10:41 -0300

    Criação do projeto WEB
    Criação do projeto WEB
    Atualização do documento

---
+++ Modelo/CosturaWEB/Costura/src/java/apoio/IDAO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apoio;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author pretto
+ */
+public interface IDAO <T> {
+
+    public String salvar (T objeto);
+
+    public String atualizar(T o);
+
+    public String excluir(int id);
+
+    public ArrayList<T> consultarTodos();
+   
+    public boolean registroUnico(T o);
+
+    public ArrayList<T> consultar(String criterio);
+
+    public Object consultarId(int id);
+    
+    public boolean consultar (T o);
+}
