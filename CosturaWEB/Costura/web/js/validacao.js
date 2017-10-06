f1c690fcf32b3062b76442cfc2b4f8c29d86bed7
Morgana <morganabagatini@gmail.com>
2017-10-05 21:56:46 -0300

    #2
    Ajustadas algumas tabelas do banco de dados

---
+++ CosturaWEB/Costura/web/js/validacao.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function validar() {
+    // so permitira o envio se os dados forem fornecidos
+
+
+    alert("meu primeiro teste");
+    document.cadastroTamanho.tamanho.focus();
+    return false;
+}
